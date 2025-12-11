60268a19fe65c28e8ac19b1b0eaa29f3f3e399f0
nimeshshrestha2023 <nimeshinfotech@gmail.com>
2025-12-11 06:21:01 +0545

    message

---
+++ Learn-HTML/JavaScript/script.js
@@ -0,0 +1,16 @@
+/* 
+EcmaScript: 
+1. Template literal
+2. Destructring(Array, object)
+3. Spread Operator
+4. Array Methods
+5. Arrow function
+*/
+
+const name = "Nimesh Shrestha";
+const age = 20;
+
+
+const result = "My name is "+name+" and age is "+age;
+
+console.log(result);
